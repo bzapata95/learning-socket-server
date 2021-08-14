d43f44f6f0d085c8745bf42906f198cb5b9b316c
Bryan Zapata <bmzc95@gmail.com>
2021-08-14 11:34:02 -0500

    alter

--- src/server.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var dotenv_1 = require("dotenv");
-var server_1 = require("./models/server");
-dotenv_1["default"].config();
-var server = new server_1["default"]();
-server.execute();
